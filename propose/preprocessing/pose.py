[FIX] rotation to camera

propose/preprocessing/pose.py
@@ -1,6 +1,8 @@
 from propose.poses import BasePose
 from propose.cameras import Camera
 
+import numpy as np
+
 
 def normalize_std(pose: BasePose) -> BasePose:
     """
@@ -20,17 +22,34 @@ def normalize_std(pose: BasePose) -> BasePose:
 def rotate_to_camera(pose: BasePose, camera: Camera):
     """
     Rotates the pose to align with the camera. i.e. if the object faces the camera it is parallel to the X axis.
+    This is achieved by deconstructing the general rotation matrix to the the yaw matrix (a.k.a RotZ matrix).
+    The yaw matrix defined as
+    [ cos(a) -sin(a) 0 ]
+    [ sin(a)  cos(a) 0 ]
+    [   0      0     1 ]
+    where "a" is the rotation angle in the XY plane.
+    "a" can be computed from the general rotation matrix (R) given it's defintion
+    [cos(a)cos(b) ... ...]
+    [sin(a)cos(b) ... ...]
+    [     ...     ... ...]
+    (Some of the terms have been omitted for brevity.)
+    Thus a = arctan( R_10 / R_00 ).
     :param pose: Pose to be rotated
     :param camera: Camera to rotate to.
     :return: rotated pose.
     """
     rot = camera.rotation_matrix.copy()
 
-    # Clean rotation matrix to be a RotZ matrix
-    rot[:, -1] = 0
-    rot[-1, :] = 0
-    rot[-1, -1] = 1
+    tan = rot[1, 0] / rot[0, 0]
+    cos = 1 / np.sqrt(1 + tan ** 2)  # Cos expressed in terms of tan
+    sin = cos * tan
 
-    rot_pose_matrix = pose.pose_matrix.dot(rot)
+    yaw = np.array([
+        [cos, -sin, 0],
+        [sin, cos, 0],
+        [0, 0, 1]
+    ])
+
+    rot_pose_matrix = pose.pose_matrix.dot(yaw)
 
     return pose.__class__(rot_pose_matrix)

tests/preprocessing/pose_test.py
@@ -5,6 +5,8 @@ from tests.mock.cameras import create_mock_camera
 
 import numpy as np
 
+from scipy.spatial.transform import Rotation as R
+
 
 def test_normalize_std():
     np.random.seed(1)
@@ -21,24 +23,29 @@ def test_normalize_std():
 
 
 def test_rotate_to_camera():
+    gen_rot = np.array([
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]
+    ])
+
+    r = R.from_matrix(gen_rot)
+    theta = np.arctan(gen_rot[0, 1] / gen_rot[0, 0])
+
     rot_matrix = np.array([
-        [1, 1, 0],
-        [1, 1, 0],
+        [np.cos(theta), -np.sin(theta), 0],
+        [np.sin(theta), np.cos(theta), 0],
         [0, 0, 1]
     ])
 
     camera_1 = create_mock_camera(rot_matrix)
-    camera_2 = create_mock_camera(np.array([
-        [1, 1, 1],
-        [1, 1, 1],
-        [1, 1, 1]
-    ]))
+    camera_2 = create_mock_camera(gen_rot)
 
     pose_matrix = np.random.random(size=(10, 20, 3))
     pose = Rat7mPose(pose_matrix)
 
     rot_pose = pp.rotate_to_camera(pose, camera_1)
-    np.testing.assert_array_equal(rot_pose.pose_matrix, pose.pose_matrix @ rot_matrix)
+    np.testing.assert_array_almost_equal(rot_pose.pose_matrix, pose.pose_matrix @ rot_matrix)
 
     rot_pose = pp.rotate_to_camera(pose, camera_2)
-    np.testing.assert_array_equal(rot_pose.pose_matrix, pose.pose_matrix @ rot_matrix)
+    np.testing.assert_array_almost_equal(rot_pose.pose_matrix, pose.pose_matrix @ rot_matrix)