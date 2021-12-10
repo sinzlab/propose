ab499238a193dd26c174188c540775c8b7b479ca
Paweł <ppierzc@gmail.com>
2021-12-10 15:27:32 +0100

    test(nn): add tests for CondGCN.message

--- tests/models/layers/CondGCN_test.py
+++ tests/models/layers/CondGCN_test.py
@@ -3,6 +3,8 @@ from propose.models.layers.CondGCN import CondGCN
 import torch
 import torch.testing as tt
 
+import types
+
 
 def test_smoke():
     CondGCN()
@@ -81,3 +83,37 @@ def test_forward():
 
     assert ('c', '->', 'x') in res_edge_index_dict
     tt.assert_equal(res_edge_index_dict[('c', '->', 'x')], edge_index_dict[('c', '->', 'x')])
+
+
+def test_message():
+    in_features = 3
+    context_features = 2
+    out_features = 3
+    hidden_features = 10
+
+    model = CondGCN(
+        in_features=in_features,
+        context_features=context_features,
+        out_features=out_features,
+        hidden_features=hidden_features
+    )
+
+    x = torch.rand(4, in_features)
+    c = torch.rand(4, context_features)
+
+    x_dict = dict(
+        x=x,
+        c=c
+    )
+
+    edge_index_dict = {('c', '->', 'x'): torch.tensor([[0, 0], [1, 1], [2, 1]]).T}
+
+    message = model.message(x_dict, edge_index_dict)
+
+    assert isinstance(message, types.GeneratorType)
+
+    for m_dst, dst in message:
+        true_dst = edge_index_dict[('c', '->', 'x')][-1]
+
+        assert m_dst.shape == (true_dst.shape[0], hidden_features)
+        tt.assert_equal(dst, edge_index_dict[('c', '->', 'x')][-1])
