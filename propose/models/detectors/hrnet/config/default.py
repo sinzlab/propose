ca731a17bff59a1add130e2d64ba399168ce9e98
paul <ppierzc@gmail.copm>
2022-10-11 11:31:12 +0200

    fix: black styling

--- propose/models/detectors/hrnet/config/default.py
+++ propose/models/detectors/hrnet/config/default.py
@@ -4,13 +4,9 @@
 # Written by Bin Xiao (Bin.Xiao@microsoft.com)
 # ------------------------------------------------------------------------------
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
+from __future__ import absolute_import, division, print_function
 
 from yacs.config import CfgNode as CN
-
 
 _C = CN()
 
--- propose/preprocessing/rat7m/__init__.py
+++ propose/preprocessing/rat7m/__init__.py
@@ -1,5 +1,4 @@
-from propose.preprocessing.rat7m.masks import *
-from propose.preprocessing.rat7m.format import *
-from propose.preprocessing.rat7m.pose import *
-
 from propose.preprocessing import *
+from propose.preprocessing.rat7m.format import *
+from propose.preprocessing.rat7m.masks import *
+from propose.preprocessing.rat7m.pose import *
