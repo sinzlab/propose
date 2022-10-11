5e67957b8bc44b271e665a8ed0b856d24817aeed
paul <ppierzc@gmail.copm>
2022-10-11 09:43:35 +0200

    add: git reproducibility.py code

--- propose/utils/reproducibility.py
+++ propose/utils/reproducibility.py
@@ -20,3 +20,24 @@ def set_random_seed(seed):
     # Make GPU operations deterministic
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+
+def get_commit_hash():
+    """
+    Returns the current commit hash shortend to 7 characters.
+    """
+    import subprocess
+
+    return subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("utf-8").strip()
+
+
+def check_uncommited_changes():
+    """
+    Checks if there are uncommited changes.
+    """
+    import subprocess
+
+    return (
+        subprocess.check_output(["git", "status", "--porcelain"]).decode("utf-8").strip()
+        != ""
+    )
